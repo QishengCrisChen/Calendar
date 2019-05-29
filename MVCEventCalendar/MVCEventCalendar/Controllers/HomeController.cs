5486175310a0a71149f646f7107f45eec561e74a
Chris Chen <cchen@resource-one.us>
2019-05-29 10:31:59 -0500

    Calendar

---
+++ MVCEventCalendar/MVCEventCalendar/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using MVCEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEventCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (OneBaseEntities2 dc = new OneBaseEntities2())
+            {
+                var events = dc.tbl_HR_PTO_Calendar.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(tbl_HR_PTO_Calendar e)
+        {
+            var status = false;
+            using (OneBaseEntities2 dc = new OneBaseEntities2())
+            {
+                if (e.ID > 0)
+                {
+                    //Update the event
+                    var v = dc.tbl_HR_PTO_Calendar.Where(a => a.ID == e.ID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Name = e.Name;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.tbl_HR_PTO_Calendar.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int ID)
+        {
+            var status = false;
+            using (OneBaseEntities2 dc = new OneBaseEntities2())
+            {
+                var v = dc.tbl_HR_PTO_Calendar.Where(a => a.ID == ID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.tbl_HR_PTO_Calendar.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ MVCEventCalendar/MVCEventCalendar/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using MVCEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEventCalendar.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Autherize(MVCEventCalendar.Models.tbl_User userModel)
+        {
+            using (OneBaseEntities1 db = new OneBaseEntities1())
+            {
+                var userDetails = db.tbl_User.Where(x => x.EmployeeNo == userModel.EmployeeNo && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["UserId"] = userDetails.UserId;
+                    Session["EmployeeNo"] = userDetails.EmployeeNo;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            //int userId = (int)Session["UserId"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
